ddceb5a3b82651b094b473711797343d74dc9cf6
Reugito <raosaheb@krypc.com>
2023-10-21 18:23:52 +0530

    first commit

---
+++ src/main/java/com/tabularize/app/TabularizeApplication.java
@@ -0,0 +1,13 @@
+package com.tabularize.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabularizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabularizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tabularize/app/TabularizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabularize.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabularizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
